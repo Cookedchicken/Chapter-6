6641c0cd34b6d7a763e882c386859b798a26b1cc
Cookedchicken <34245870+Cookedchicken@users.noreply.github.com>
2019-12-13 14:16:08 -0600

    chapter 666

---
+++ ArrayDemo/ArrayDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ArrayDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int howMany;
+            Console.WriteLine("How many numbers are you going to enter?");
+            howMany = Convert.ToInt32(Console.ReadLine());
+            int[] eightNums = new int[howMany];
+            int entry;
+            for (int i = 0; i < eightNums.Length; ++i)
+            {
+                Console.WriteLine("Please enter a number >>>>");
+                eightNums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("How do you want to see the information\n"+
+                "1) First to Last?\n" +
+                "2) Last to first?\n"+
+                "3) Choose a specific position");
+            entry = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+        public static void DisplayFirstToLast()
+        {
+
+        }
+    }
+}
---
+++ testScores/testScores/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace testScores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] scores = new int[8];
+            int input;
+            for (int i = 0; i < scores.Length; ++i)
+            {
+                Console.WriteLine("Plese enter a test score #" + (i+1));
+                input = Convert.ToInt32(Console.ReadLine());
+                scores[i] = input;
+            }
+            RunLine();
+            DisplayScores(scores);
+
+        }
+        
+        public static void DisplayTotal(int[] scores)
+        {
+            int total = 0;
+            for(int i = 0; i < scores.Length; ++i)
+            {
+                total = total + scores[i];
+            }
+            RunLine();
+            Console.WriteLine(total);
+        }
+        public static void DisplayScores(int[]scores)
+        {
+            Console.WriteLine("Scores in original order");
+            for (int i = 0; i < scores.Length; ++i)
+            {
+                Console.WriteLine("{0, 6}" , scores[i]);
+            }
+        }
+        public static void RunLine()
+        {
+            Console.WriteLine("\n------------------------------------------------");
+        }
+    }
+}
